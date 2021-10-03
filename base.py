1e2e50217eee24f2ba65a61f14c63243fafdb43d
Serge <shumilovru2@gmail.com>
2021-10-03 09:34:28 +0300

    Version with completed RIA and RBK parsers, bot works properly and returns news for these resources

---
+++ base.py
@@ -0,0 +1,55 @@
+import logging
+import config as cfg
+from parser_classes import RbcParse, RiaParse
+
+from aiogram import Bot, Dispatcher, executor, types
+
+# log level
+logging.basicConfig(level=logging.INFO)
+
+# Bot initialisation
+bot = Bot(token=cfg.API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands='start')
+async def start_msg(message: types.Message):
+    await message.answer("Hey, what news are you looking for? Choose a resource)")
+
+
+@dp.message_handler(commands='rbk')
+async def get_rbk(message: types.Message):
+    await message.answer("What is your key word?")
+
+    @dp.message_handler(content_types=types.ContentType.TEXT)
+    async def get_rbk_news(message: types.Message):
+        k_word = message.text
+
+        rbc = RbcParse(k_word=k_word).rbc_parse()
+        if isinstance(rbc, dict):
+            for title in rbc:
+                await message.answer(f'{title}. \n --> Read more: {rbc[title]}')
+
+        elif isinstance(rbc, str):
+            await message.answer(rbc)
+
+
+@dp.message_handler(commands='ria')
+async def get_rbk(message: types.Message):
+    await message.answer("What is your key word?")
+
+    @dp.message_handler(content_types=types.ContentType.TEXT)
+    async def get_ria_news(message: types.Message):
+        k_word = message.text
+
+        ria = RiaParse(k_word=k_word).ria_parse()
+        if isinstance(ria, dict):
+            for title in ria:
+                await message.answer(f'{title}. \n --> Read more: {ria[title]}')
+
+        elif isinstance(ria, str):
+            await message.answer(ria)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
---
+++ parser_classes.py
@@ -0,0 +1,87 @@
+"""Парсер сайтов на основе классов для новостного ТГ бота"""
+import config
+import requests
+from bs4 import BeautifulSoup
+
+links = config.resources  # список ссылок на ресурсы
+
+# заголовки для иммитации реального пользователя
+headers = {
+    "accept": "*/*",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36"
+}
+
+
+def titles_pars(titles):
+    """Поиск и добавление заголовков в список"""
+    if titles:
+        title_list = []  # список чистых заголовков
+
+        for title in titles:
+            title_list.append(title.text)  # получаем текст заголовка
+
+        return title_list
+
+
+def links_parse(articles):
+    article_list = []  # список ссылок на статьи
+
+    for article in articles:
+        article_url = article.get("href")  # получаем ссылку из тега
+        article_list.append(article_url)
+
+    return article_list
+
+
+class MyParser:
+
+    def __init__(self, resource, key_word):
+        self.url = links[resource] + key_word  # собираем ссылку с поисковым запросом
+        self.req = requests.get(self.url, headers=headers)
+        self.src = self.req.text
+        self.soup = BeautifulSoup(self.src, 'lxml')
+
+
+class RbcParse(MyParser):
+
+    def __init__(self, k_word):
+        super().__init__(0, key_word=k_word)
+        # Собираем заголовки со страницы РБК
+        self.titles = self.soup.find_all("span", class_="search-item__title", limit=5)
+        # Собираем ссылки на статьи со страницы РБК
+        self.articles = self.soup.find_all("a", class_='search-item__link', limit=5)
+
+    def rbc_parse(self):
+
+        title_list = titles_pars(self.titles)
+        if self.titles:
+            article_list = links_parse(self.articles)
+
+            dicty = dict(zip(title_list, article_list))  # создаем словарь {заголовок: ссылка}
+
+            return dicty
+
+        elif not self.titles:
+            return "No articles with this word today on RBK"
+
+
+class RiaParse(MyParser):
+
+    def __init__(self, k_word):
+        super().__init__(1, key_word=k_word)
+        # Собираем заголовки со страницы РИА
+        self.titles = self.soup.find_all("a", class_="list-item__title", limit=5)
+        # Собираем ссылки на статьи со страницы РИА
+        self.articles = self.soup.find_all("a", class_="list-item__title", limit=5)
+
+    def ria_parse(self):
+        title_list = titles_pars(self.titles)
+        if self.titles:
+            article_list = links_parse(self.articles)
+
+            dicty = dict(zip(title_list, article_list))  # создаем словарь {заголовок: ссылка}
+
+            return dicty
+
+        elif not self.titles:
+            return "No articles with this word today on RIA"
